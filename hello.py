36dbc30ce6469d1038ab97d948cc8cc86b10ae9e
Rose Reyes <roseannlorinreyes@gmail.com>
2018-04-26 01:06:55 -0700

    added flask exercises in python

---
+++ hello.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app = Flask(__name__)    # Global variable __name__ tells Flask whether or not we are running the file
+                         # directly, or importing it as a module.
+@app.route('/')          # The "@" symbol designates a "decorator" which attaches the following
+                         # function to the '/' route. This means that whenever we send a request to
+                         # localhost:5000/ we will run the following "hello_world" function.
+def hello_world():
+  return render_template('index.html')  
+
+@app.route('/success')
+def success():
+  return render_template('success.html')
+app.run(debug=True)
